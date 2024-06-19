5c371644725a173533e83b5bebef54aa67ebc8b7
Gokul <gokulmodi369@gmail.com>
2024-06-19 23:08:10 +0530

    shift and unshift  polyfill done with example

--- push.js
+++ push.js
@@ -19,7 +19,31 @@ Array.prototype.myPop = function () {
     return ans;
 }
 // arr.pop()
-arr.myPop()
+// arr.myPop()
 // console.log(ans2)
+
+Array.prototype.myShift = function () {
+    let ans = this[0];
+    for (let i = 0; i < this.length - 1; i++) {
+        this[i] = this[i + 1];
+    }
+    delete this[this.length - 1];
+    this.length--;
+    return ans;
+}
+
+Array.prototype.myUnShift = function () {
+    for (let i = this.length - 1; i >= 0; i--) {
+        this[i + 1] = this[i];
+    }
+    for (let i = 0; i < arguments.length; i++) {
+        this[i] = arguments[i];
+    }
+    this.length += arguments.length;
+    return this.length;
+}
+
+arr.myShift()
+
 console.log(arr)
 console.log(arr.length)
